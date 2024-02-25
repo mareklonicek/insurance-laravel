92d9a6b11e931861ebaa1d6597683327a95fc9e8
Marek Lonicek <mark.lonicek@seznam.cz>
2024-02-25 09:01:33 +0100

    Update Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,17 +1,1 @@
-<?php
 
-namespace App\Models;
-
-use App\Models\Insuree;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-
-class Category extends Model
-{
-    use HasFactory;
-    protected $guarded = [];
-    public function insurees()
-    {
-        return $this->belongsToMany(Insuree::class);
-    }
-}
